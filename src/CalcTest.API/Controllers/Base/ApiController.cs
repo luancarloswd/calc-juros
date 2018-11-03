c8bac4835e69694309c0f81323954067f38cc5cc
Luan Carlos Sousa Santos <contato@luancarlos.com.br>
2018-11-03 12:57:31 -0300

    Criando Controller base

---
+++ src/CalcTest.API/Controllers/Base/ApiController.cs
@@ -0,0 +1,50 @@
+﻿using CalcTest.Crosscutting.Interfaces;
+using CalcTest.Crosscutting.Notifications;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CalcTest.API.Controllers.Base
+{
+    public abstract class ApiController : ControllerBase
+    {
+        private readonly IDomainNotificationManager _domainNotificationManager;
+
+        protected ApiController(IDomainNotificationManager domainNotificationManager)
+            => _domainNotificationManager = domainNotificationManager;
+
+        protected IEnumerable<DomainNotification> Notifications => _domainNotificationManager.GetNotifications();
+
+        protected bool IsValidOperation()
+            => (!_domainNotificationManager.HasNotifications());
+
+        protected new IActionResult Response(object result = null)
+        {
+            if (IsValidOperation())
+                return Ok(new
+                {
+                    success = true,
+                    data = result
+                });
+
+            return BadRequest(new
+            {
+                success = false,
+                data = result,
+                errors = _domainNotificationManager.GetNotifications()
+            });
+        }
+
+        protected void NotifyModelStateErrors()
+        {
+            foreach (var ms in ModelState)
+                foreach (var erro in ms.Value.Errors)
+                {
+                    var erroMsg = erro.Exception == null ? erro.ErrorMessage : erro.Exception.Message;
+                    NotifyError(ms.Key, erroMsg);
+                }
+        }
+
+        protected void NotifyError(string code, string message)
+            => _domainNotificationManager.Add(code, message);
+    }
+}
