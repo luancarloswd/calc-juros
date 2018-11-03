b67d04a94ae0a8138291ca9cd9c81856823b8f70
Luan Carlos Sousa Santos <contato@luancarlos.com.br>
2018-11-03 13:37:04 -0300

    Criando controller ShomeTheCode

---
+++ src/CalcTest.API/Controllers/ShowMeTheCode.cs
@@ -0,0 +1,17 @@
+﻿using CalcTest.API.Controllers.Base;
+using CalcTest.Crosscutting.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalcTest.API.Controllers
+{
+    [ApiVersion("1")]
+    public class ShowMeTheCode : ApiController
+    {
+        public ShowMeTheCode(IDomainNotificationManager domainNotificationManager) : base(domainNotificationManager)
+        { }
+
+        [Route("showmethecode")]
+        public IActionResult Get()
+            => Response("https://github.com/luancarloswd/softplayer/");
+    }
+}
