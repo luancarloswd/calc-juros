fdb8d44c14dfab952c223970d5e8129e1bd564eb
Luan Carlos Sousa Santos <contato@luancarlos.com.br>
2018-11-03 11:48:24 -0300

    Adicionando automapper + mapeado domínio para vms

---
+++ src/CalcTest.Service/Automapper/AutoMapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalcTest.Service.Automapper
+{
+    public class AutoMapperConfig
+    {
+        public static MapperConfiguration RegisterMappings()
+         => new MapperConfiguration(cfg =>
+         {
+             cfg.AddProfile(new ViewModelToDomainMappingProfile());
+             cfg.AddProfile(new DomainToViewModelMappingProfile());
+         });
+    }
+}
