24dd73b168c3432c7eb72afc66edc8867ebe4e4f
Luan Carlos Sousa Santos <contato@luancarlos.com.br>
2018-11-03 12:02:15 -0300

    Criando projeto para injeção de dependências

---
+++ src/CalcTest.IoC/NativeInjectorBootStrapper.cs
@@ -0,0 +1,17 @@
+﻿using CalcTest.Crosscutting.Interfaces;
+using CalcTest.Crosscutting.Notifications;
+using CalcTest.Service.Interfaces;
+using CalcTest.Service.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CalcTest.IoC
+{
+    public class NativeInjectorBootStrapper
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<IDomainNotificationManager, DomainNotificationManager>();
+            services.AddScoped<IJurosCompostosService, JurosCompostosService>();
+        }
+    }
+}
